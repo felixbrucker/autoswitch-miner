28a0281b7f700070451f5ac8edd905149a10817e
Felix Brucker <felix.brucker@live.de>
2016-08-07 01:26:49 +0200

    stuffs

--- api/controllers/miningController.js
+++ api/controllers/miningController.js
@@ -360,6 +360,7 @@ function checkBenchmark(req, res, next) {
 
 function init() {
   getProfitability();
+  getMinerStats();
   if (configModule.config.autostart) {
     console.log(colors.blue("autostart enabled, starting miner shortly.."));
     setTimeout(function () {
