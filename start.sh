58f530706e200627464f2fdae854a9a828072aca
Felix Brucker <felix.brucker@live.de>
2016-08-24 23:04:37 +0200

    - stuffs

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 prevVersion=`git submodule status`
 git pull
-git submodule update --recursive
+git submodule update --recursive --remote
 currVersion=`git submodule status`
 if [ "$prevVersion" != "$currVersion" ]; then
   cd cpuminer-opt
