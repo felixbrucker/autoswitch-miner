75d80591034069c4915f19ab40d69fdee7ddcd0c
Felix Brucker <felix.brucker@live.de>
2016-08-25 13:10:35 +0200

    - stuffs

--- start.sh
+++ start.sh
@@ -15,6 +15,11 @@ if [ "$prevVersion1" != "$currVersion1" ]; then
 fi
 if [ "$prevVersion2" != "$currVersion2" ]; then
   cd cpuminer-multi
+  # ugly fix
+  sed -i -- 's/lyra2.h/Lyra2.h/g' lyra2/Lyra2.c
+  sed -i -- 's/lyra2.h/Lyra2.h/g' lyra2/Sponge.c
+  sed -i -- 's/sponge.h/Sponge.h/g' lyra2/Lyra2.c
+  sed -i -- 's/sponge.h/Sponge.h/g' lyra2/Sponge.c
   ./build.sh
   mkdir -p ../bin
   cp cpuminer ../bin/cpuminer-multi
