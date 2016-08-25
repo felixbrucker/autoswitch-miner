1596f534f96026a8944540e99a2b92a008aab9e4
Felix Brucker <felix.brucker@live.de>
2016-08-25 13:04:25 +0200

    - stuffs

--- start.sh
+++ start.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-prevVersion=`git submodule status`
+prevVersion1=`git submodule status cpuminer-opt`
+prevVersion2=`git submodule status cpuminer-multi`
 git pull
 git submodule update --recursive --remote
-currVersion=`git submodule status`
-if [ "$prevVersion" != "$currVersion" ]; then
+currVersion1=`git submodule status cpuminer-opt`
+currVersion2=`git submodule status cpuminer-multi`
+if [ "$prevVersion1" != "$currVersion1" ]; then
   cd cpuminer-opt
   ./build.sh
   mkdir -p ../bin
   cp cpuminer ../bin/
+  git reset --hard
+fi
+if [ "$prevVersion2" != "$currVersion2" ]; then
+  cd cpuminer-multi
+  ./build.sh
+  mkdir -p ../bin
+  cp cpuminer ../bin/cpuminer-multi
   git reset --hard
 fi
 npm update
