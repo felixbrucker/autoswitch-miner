c306284e349c3167f705392f13e4025a428ee95a
Felix Brucker <felix.brucker@live.de>
2016-09-13 22:41:00 +0200

    ccminer init

--- api/modules/configModule.js
+++ api/modules/configModule.js
@@ -67,7 +67,7 @@ var config = module.exports = {
   },
   saveConfig: function () {
     console.log(colors.grey("writing config to file.."));
-    fs.writeFile(configPath, JSON.stringify(config.config), function (err) {
+    fs.writeFile(configPath, JSON.stringify(config.config,null,2), function (err) {
       if (err) {
         return console.log(err);
       }
